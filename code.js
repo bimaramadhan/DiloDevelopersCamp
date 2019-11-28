59e65809bae939735ae212a8126fd5942ff57913
bimaramadhan <bimarmdhan@gmail.com>
2019-11-28 13:51:32 +0700

    latihan javascript

---
+++ code.js
@@ -0,0 +1,79 @@
+// let status = 'friendzone';
+
+// if(status === 'jomblo') {
+//     console.log('banyak duit');
+// } else if(status === 'friendzone') {
+//     console.log('bahagia :(');
+// } else
+//     console.log('kantong kosong');
+
+// let name = 'John';
+// name = 'Doe';
+// console.log(name);
+
+// const firstName = 'Bima';
+// firstName = 'Satria'
+// console.log(firstName);
+
+// let name = "John";
+// let getName = function() {
+//     console.log(name);
+
+//     let age = 25;
+// }
+
+// getName();
+// console.log(age);
+
+// let firstName = 'Bima'
+// let lastName = 'Satria';
+// // let fullName = 
+// // 'Nama Saya ' + firstName + ' ' + lastName
+
+// let getAge = function() {
+//     return 20;
+// }
+
+// let fullName = `
+//     Nama Saya
+//     ${firstName} ${lastName}
+//     Usia Saya : ${getAge() - 2}
+// `;
+// console.log(fullName);
+
+// let myBook = {
+//     title: 'How I Meet Your Mother',
+//     author: 'John F. Doe',
+//     pageCount: 326
+// }
+
+// let getBook = function(book) {
+//     console.log(`Judul Buku ${book.title}`);
+// }
+// getBook(myBook);
+
+// console.log(`
+//     Judul Buku  : ${myBook.title}
+//     Pengarang   : ${myBook.author}
+//     Jumlah Halaman  : ${myBook.pageCount}
+// `);
+
+// let myAccount = {
+//     name: 'John Doe',
+//     expense: 0,
+//     income: 0
+// }
+
+// let addExpense = function(account, amount) {
+//     console.log(account)
+//     account.expense = account.expense + amount
+//     console.log(account)
+// }
+
+// addExpense(myAccount, 500)
+// console.log(myAccount)
+
+let name = 'John Doe'
+
+console.log(name.)
+
